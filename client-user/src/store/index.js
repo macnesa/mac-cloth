f9ad247beb3c6e5300ee27ec0efd1c942b206a79
mmacnesa <mmacnesa@gmail.com>
2023-02-18 08:30:11 +0700

    commit

--- client-user/src/store/index.js
+++ client-user/src/store/index.js
@@ -8,4 +8,29 @@ const store = createStore(rootReducer, applyMiddleware(logger, thunk))
 //   console.log(store.getState())
 // })
 
-export default store 
+
+
+
+export default store
+
+
+
+
+
+
+
+
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(require('./routes'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port port`)
+})
